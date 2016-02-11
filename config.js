e947d96f6e9ce83b2f45fb4fcce09057c938b34d
Maxime Bret <maxime.bret.ext@boursorama.fr>
2016-02-11 14:38:52 +0100

    no message

--- config.js
+++ config.js
@@ -11,10 +11,6 @@ module.exports = {
      */
     sleepTime: ["07:00", "08:00"],
 
-    log: {
-        level: 'debug'
-    },
-
     // Override default user module path
     // By default, the module loader will lookup by module name
     externalModuleRepositories: [],
@@ -27,6 +23,35 @@ module.exports = {
                 clientSecret: null
             }
         }
-    }
+    },
 
+    tmpDir: '.tmp',
+
+    log: {
+        level: 'debug'
+    },
+
+    //coreModulesPath: '/lib/core_modules',
+
+    // User modules to activate
+    // Handled by app
+    activeModules: [],
+
+    // The list of actions a task can do.
+    // This list will be filled dynamically with core module
+    // For example a core module can add the action [mail].
+    // Then a task can be created with action [mail]. The core module
+    // will listen for [mail] event and do things ...
+    taskActions: {
+
+    },
+
+    tasks: [],
+
+    // Persistance relative
+    persistancePath: '/data/storage',
+    databasePath: 'persistence.db',
+    taskDatabase: 'tasks.db',
+    //messagesAdaptersDb: 'messagesAdapters.db',
+    notificationsDb: 'notifications.db'
 };
