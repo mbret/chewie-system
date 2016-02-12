ea1668df57edbcc52dd25738c6731c37b59f84f9
Maxime Bret <maxime.bret.ext@boursorama.fr>
2016-02-12 15:22:27 +0100

    no message

--- config.js
+++ config.js
@@ -15,16 +15,6 @@ module.exports = {
     // By default, the module loader will lookup by module name
     externalModuleRepositories: [],
 
-    externalServices: {
-        google: {
-            auth: {
-                // Should be provided in config local
-                clientId: null,
-                clientSecret: null
-            }
-        }
-    },
-
     tmpDir: '.tmp',
 
     log: {
