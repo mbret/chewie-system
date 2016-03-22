3ad21026ee665cab7c6b7165a5d4331a5ec1f430
Maxime Bret <maxime.bret.ext@boursorama.fr>
2016-03-22 14:18:58 +0100

    no message

--- _dev/gulpfile.js
+++ _dev/gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var install = require("gulp-install");
 
 gulp.task('install_dev_plugins', function(){
-    gulp.src(['./_dev_plugins/*/package.json'])
+    gulp.src([__dirname + '/plugins/*/package.json'])
         .pipe(install());
 });
 
 gulp.task('install_dev', ['install_dev_plugins'], function(){
-    gulp.src(['./package.json'])
+    gulp.src([__dirname + '/../package.json'])
         .pipe(install());
 });
