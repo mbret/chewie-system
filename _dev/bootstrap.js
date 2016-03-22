976cb15e186b883121c6b73782cdb85848118193
Maxime Bret <maxime.bret.ext@boursorama.fr>
2016-03-22 19:33:11 +0100

    no message

--- _dev/bootstrap.js
+++ _dev/bootstrap.js
@@ -3,13 +3,30 @@
 module.exports = function(system, logger, done){
     system.orm.models.User.findOne({where: {username: 'admin'}})
         .then(function(user){
-            return system.orm.models.Plugins.findOrCreate({
-                where: {id:'simple-message'},
-                defaults: {
-                    id: 'simple-message',
-                    userId: user.id
-                }
-            });
+            // extract plugin
+            return system.localRepository.loadPlugin('simple-message')
+                .then(function(module, packageInfo){
+                    // create plugin
+                    return system.orm.models.Plugins
+                        .findOrCreate({
+                            where: {package:'simple-message'},
+                            defaults: {
+                                package: 'simple-message',
+                                userId: user.id
+                            }
+                        })
+                        // Create module
+                        .then(function(plugin){
+                            return system.orm.models.Modules.findOrCreate({
+                                where: {type: 'task-module', name: 'simple-message', pluginId: plugin[0].id},
+                                defaults: {
+                                    type: 'task-module',
+                                    name: 'simple-message',
+                                    pluginId: plugin[0].id
+                                }
+                            })
+                        });
+                });
         })
         .then(function(){
             return done();
