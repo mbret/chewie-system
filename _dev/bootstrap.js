a27b6a9156a33b9ce2cfe4227d11fa36edc60a86
Maxime Bret <maxime.bret.ext@boursorama.fr>
2016-03-30 19:44:29 +0200

    no message

--- _dev/bootstrap.js
+++ _dev/bootstrap.js
@@ -27,18 +27,21 @@ module.exports = function(system, logger, done){
                         .then(function(packages){
                             var packageJson = packages[0];
                             var pluginPackage = packages[1];
+
                             // create plugin
-                            return system.orm.models.Plugins.findOrCreate({
-                                where: {name:'keypress-trigger', userId: user.id},
-                                defaults: {
-                                    modulePackage: packageJson,
-                                    pluginPackage: pluginPackage,
-                                    version: packageJson.version,
-                                    description: packageJson.description,
-                                    name: packageJson.name,
-                                    userId: user.id
-                                }
-                            });
+                            return Promise.all([
+                                system.orm.models.Plugins.findOrCreate({
+                                    where: {name:'keypress-trigger', userId: user.id},
+                                    defaults: {
+                                        modulePackage: packageJson,
+                                        pluginPackage: pluginPackage,
+                                        version: packageJson.version,
+                                        description: packageJson.description,
+                                        name: packageJson.name,
+                                        userId: user.id
+                                    }
+                                })
+                            ]);
                         });
                 })
                 .then(function(){
@@ -50,14 +53,33 @@ module.exports = function(system, logger, done){
                             options: { foo: 'bar' },
                             userId: user.id,
                             triggers: [
-                                {
-                                    type: 'direct',
-                                    options: {'taskOptions.option1': 'coucou'},
-                                },
+                                //{
+                                //    type: 'direct',
+                                //    options: {'taskOptions.option1': 'coucou'},
+                                //},
+                                //{
+                                //    type: 'schedule',
+                                //    options: {'taskOptions.option1': 'coucou'},
+                                //    schedule: {
+                                //        method: 'interval',
+                                //        interval: 1000
+                                //    }
+                                //},
+                                //{
+                                //    type: 'schedule',
+                                //    options: {'taskOptions.option1': 'coucou'},
+                                //    schedule: {
+                                //        method: 'moment',
+                                //        date: new Date()
+                                //    }
+                                //}
                                 {
                                     type: 'schedule',
                                     options: {'taskOptions.option1': 'coucou'},
-                                    schedule: {}
+                                    schedule: {
+                                        method: 'moment',
+                                        date: new Date()
+                                    }
                                 }
                             ]
                         })
