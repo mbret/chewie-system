07d0879fe7f4861e9328284c6deccc3afba167db
Maxime Bret <Maxime Bret>
2016-06-17 01:35:14 +0200

    no message

--- _dev-linux-blank/config/default.js
+++ _dev-linux-blank/config/default.js
@@ -5,6 +5,13 @@ var path = require('path');
 module.exports = {
 
     system: {
-
+        speakerAdapter: {
+            // module: require('my-buddy-lib').speakerAdapter,
+            module: require("my-buddy-lib").speakerAdapter,
+            options: {
+                // use mpg123 by default
+                mpg123BinaryPath: "mpg123"
+            }
+        }
     }
 };
