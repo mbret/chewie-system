4cf71b45e6343b3691c5ac75fc5f5529afcbc018
Maxime Bret <Maxime Bret>
2016-09-14 23:09:26 +0200

    no message

---
+++ _dev/_dev-linux/config/default.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = {
+
+    profileToLoadOnStartup: "admin",
+
+    system: {
+        speakerAdapter: {
+            // module: require('my-buddy-lib').speakerAdapter,
+            module: require("my-buddy-lib").speakerAdapter,
+            options: {
+                // use mpg123 by default
+                mpg123BinaryPath: ""
+            }
+        }
+    },
+
+    plugins: {
+        localRepositories: [process.env.APP_ROOT_PATH + '/../plugins']
+    },
+};
--- _dev/plugins/radio/module.js
+++ _dev/plugins/radio/module.js
@@ -21,16 +21,50 @@ class Module {
         class Task extends EventEmitter {
 
         }
-        var task = new Task();
-        task.options = {radioName: "nrj"};
+        var task = null;
 
+        // morning start Yann
         var rule = new scheduler.RecurrenceRule();
-        rule.hour = 9;
-        rule.minute = 0;
+        rule.hour = 7;
+        rule.minute = 40;
         rule.dayOfWeek = [1,2,3,4,5];
         scheduler.scheduleJob(rule, function(){
+            task = new Task();
+            task.options = {radioName: "nrj"};
             self.newTask(task);
         });
+
+        // morning end Yann
+        var rule2 = new scheduler.RecurrenceRule();
+        rule2.hour = 8;
+        rule2.minute = 30;
+        rule2.dayOfWeek = [1,2,3,4,5];
+        scheduler.scheduleJob(rule2, function(){
+            task.emit("stop");
+        });
+
+        // ================================
+
+        // morning start Max
+        var c = new scheduler.RecurrenceRule();
+        c.hour = 9;
+        c.minute = 0;
+        c.dayOfWeek = [1,2,3,4,5];
+        scheduler.scheduleJob(c, function(){
+            task = new Task();
+            task.options = {radioName: "nrj"};
+            self.newTask(task);
+        });
+
+        // morning end Max
+        var d = new scheduler.RecurrenceRule();
+        d.hour = 9;
+        d.minute = 40;
+        d.dayOfWeek = [1,2,3,4,5];
+        scheduler.scheduleJob(d, function(){
+            task.emit("stop");
+        });
+
         // @todo fin dev
 
         return cb();
