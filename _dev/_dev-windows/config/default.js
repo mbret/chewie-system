bd0295dd46e9fe9be90ab3bf3f919f7fc08dd09d
Maxime Bret <maxime.bret.ext@boursorama.fr>
2016-10-19 20:35:01 +0200

    no message

--- _dev/_dev-windows/config/default.js
+++ _dev/_dev-windows/config/default.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 module.exports = {
 
-    //profileToLoadOnStartup: "admin",
+    profileToLoadOnStartup: "admin",
     
     system: {
         tmpDir:         path.join(__dirname, '../.my-buddy/tmp'),
