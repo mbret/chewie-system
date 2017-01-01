f1d07b55be5a28d5b313cf2964b2cfaff10d4687
DESKTOP-64PLAOK\maxime <bret.maxime@gmail.com>
2017-01-01 20:17:14 +0100

    no message

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,7 @@
 
 import {System} from "./system";
 
+console.log("fuck");process.exit();
 // Read the ctrl+c of windows to handle SIGINT correctly
 if (process.platform === "win32") {
     let rl = require("readline")
