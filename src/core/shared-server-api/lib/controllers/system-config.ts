b7f1ae1b52852660f5238b4f94c7334ec7684bc9
DESKTOP-64PLAOK\maxime <bret.maxime@gmail.com>
2017-03-27 02:14:02 +0200

    no message

--- src/core/shared-server-api/lib/controllers/system-config.ts
+++ src/core/shared-server-api/lib/controllers/system-config.ts
@@ -6,46 +6,46 @@ const validator = require('validator');
 
 module.exports = function(server, router) {
 
-    router.put("/devices/:device/hooks-config/:hook", function(req, res) {
-        let HookOptionDao = server.orm.models.HookOption;
-        let deviceId = req.params.device;
-        let hookName = req.params.hook;
-        let newData = req.body.options;
-
-        // validate body
-        let errors = {};
-
-        if(!_.isEmpty(errors)){
-            return res.badRequest(errors);
-        }
-
-        HookOptionDao
-            .findOne({ where: {
-                deviceId: deviceId,
-                hookName: hookName,
-            } })
-            .then(function(entry) {
-                // Create
-                if(!entry){
-                    let data = { deviceId: deviceId, hookName: hookName, data: newData };
-                    return HookOptionDao
-                        .create(data)
-                        .then(function(created) {
-                            server.logger.verbose("Hook config for [%s] created with id [%s]", hookName, created.id);
-                            server.io.emit("hooks-config:created", created.toJSON());
-                            return res.created(created.toJSON());
-                        });
-                }
-                // Update
-                return entry
-                    .update({deviceId: req.params.device, hookName: hookName, data: newData})
-                    .then(function(entryUpdated) {
-                        server.logger.verbose("Hook config for [%s] with id [%s] updated", hookName, entryUpdated.id);
-                        server.io.emit("hooks-config:updated", entryUpdated.toJSON());
-                        return res.ok(entryUpdated.toJSON());
-                    });
-            })
-            .catch(res.serverError);
+    router.put("/system-config/:device", function(req, res) {
+        // let HookOptionDao = server.orm.models.HookOption;
+        // let deviceId = req.params.device;
+        // let hookName = req.params.hook;
+        // let newData = req.body.options;
+        //
+        // // validate body
+        // let errors = {};
+        //
+        // if(!_.isEmpty(errors)){
+        //     return res.badRequest(errors);
+        // }
+        //
+        // HookOptionDao
+        //     .findOne({ where: {
+        //         deviceId: deviceId,
+        //         hookName: hookName,
+        //     } })
+        //     .then(function(entry) {
+        //         // Create
+        //         if(!entry){
+        //             let data = { deviceId: deviceId, hookName: hookName, data: newData };
+        //             return HookOptionDao
+        //                 .create(data)
+        //                 .then(function(created) {
+        //                     server.logger.verbose("Hook config for [%s] created with id [%s]", hookName, created.id);
+        //                     server.io.emit("hooks-config:created", created.toJSON());
+        //                     return res.created(created.toJSON());
+        //                 });
+        //         }
+        //         // Update
+        //         return entry
+        //             .update({deviceId: req.params.device, hookName: hookName, data: newData})
+        //             .then(function(entryUpdated) {
+        //                 server.logger.verbose("Hook config for [%s] with id [%s] updated", hookName, entryUpdated.id);
+        //                 server.io.emit("hooks-config:updated", entryUpdated.toJSON());
+        //                 return res.ok(entryUpdated.toJSON());
+        //             });
+        //     })
+        //     .catch(res.serverError);
     });
 
     router.get("/system-config/:device", function(req, res) {
